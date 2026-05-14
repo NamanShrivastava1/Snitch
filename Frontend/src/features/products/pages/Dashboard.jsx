034ca40f6df894b43bf30caa71bab1b4ebad0e64
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-05-14 14:47:54 +0530

    feat: add SellerProductDetails page and update dashboard navigation route for sellers

--- Frontend/src/features/products/pages/Dashboard.jsx
+++ Frontend/src/features/products/pages/Dashboard.jsx
@@ -103,7 +103,7 @@ const Dashboard = () => {
                 return (
                   <div
                     onClick={() => {
-                      navigate(`/product/${product._id}`);
+                      navigate(`/seller/product/${product._id}`);
                     }}
                     key={product._id}
                     className="group cursor-pointer flex flex-col"
