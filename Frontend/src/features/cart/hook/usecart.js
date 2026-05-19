4bf351a3ddfafa5984d91fffcc918fb591203970
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-05-19 15:11:10 +0530

    feat: implement cart item quantity increment functionality across API, state, and hook

--- Frontend/src/features/cart/hook/usecart.js
+++ Frontend/src/features/cart/hook/usecart.js
@@ -1,6 +1,10 @@
-import { addItem, getCart } from "../service/cart.api";
+import { addItem, getCart, incrementCartItemApi } from "../service/cart.api";
 import { useDispatch } from "react-redux";
-import { addItem as addItemToCart, setItems } from "../state/cart.slice";
+import {
+  addItem as addItemToCart,
+  setItems,
+  incrementCartItem,
+} from "../state/cart.slice";
 
 export const useCart = () => {
   const dispatch = useDispatch();
@@ -16,5 +20,11 @@ export const useCart = () => {
     return data.cart.items;
   }
 
-  return { handleAddItem, handleGetCart };
+  async function handleIncrementCartItem({ productId, variantId }) {
+    const data = await incrementCartItemApi(productId, variantId);
+    dispatch(incrementCartItem({ productId, variantId }));
+    return data;
+  }
+
+  return { handleAddItem, handleGetCart, handleIncrementCartItem };
 };
