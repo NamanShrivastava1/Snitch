6ae96f4cb5bb706c684a4e614b1a8ddf56e02469
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-07-30 11:20:06 +0530

    refactor: comment out proxy configuration in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,13 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // Only for Proxy
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3000",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
